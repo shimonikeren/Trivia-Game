450d186bdbf3148d57ae190f0c39f19289eae6ab
unknown <kerenshimoni6@gmail.com>
2018-02-21 11:05:57 -0800

    complete most of the logic

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Trivia-Game
+
+
+//note to self: change timer to 20 sec
+//need to figutr out how to loop through all questions 
+//THEN when all questions have been asked and answered, make the final screen in result box
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,93 +1,105 @@
+//things I still need to consider: 
+//stop letting timer go below zero
+//rendering questions, increment pos variable ---WHERE TO PUT pos ++ ????
+//fade in/out the timer text
+//
+// =================Declare Global vars and arrays =================
 correct = 0;
 incorrect = 0;
 unanswered = 1;
 var timerInterval;
-var number =10;
-var pos = 0, test, test_status, question, choice, choices, chA, chB, chC, chD, correct=0;
+var number = 11;
+var pos = 0,
+	test, question, choice, choices, chA, chB, chC, chD, correct = 0;
 var questions = [
-	//each of the following arrays are formatted as follows: [question, choice1, choice2, ch3, ch4, answer]
+	//multidimensional array: each formatted as follows: [question, chA(choiceA), chB, chC, chD, answer]
 	["where is dijon from?", "England", "France", "USA", "Japan", "B"],
 	["what is yummiest?", "Chocolate", "Celery", "cucumber", "corn", "A"],
-	["Does pizza have cheese?", "no", "never", "nope", "yes", "D"]
+	["Does pizza have cheese?", "no", "never", "nope", "yes", "D"],
+	["where is PIZZA from?", "England", "ITALY", "USA", "Japan", "B"]
 ];
 
-$('document').ready(function(){
+// =================Declare Functions =================
 
+$('document').ready(function () {
 
-$(".start").on("click", start)
-function start (){
-	function runTimer() {
-  		timerInterval = setInterval(decrement, 1000);
-	}	
-		function decrement() {
-	  	number--;
-	  	$("#timer").html("<h2>" + number + "</h2>");
-
-		  	if (number === 0) {
-			$("#timeEnd").html("Timer: You ran out of time! Try Again!").css('color', 'red');
-			$("#unanswered").text("Unanswered Questions: " + unanswered++);
-			clearInterval(timerInterval);
-			}
+	function runTimer() { //function to run a timer, decrease by 1 second 
+		timerInterval = setInterval(decrement, 1000);
 	}
-	runTimer();
 
-function reset (){
-	clearInterval(timerInterval);
-	number = 10;
-	start();
-}
+	function decrement() { //function for seconds decreasing by 1  
+		number--;
+		$("#timer").html("<h2>" + number + "</h2>");
+		if (number === 0) {
+			timesUp();
+		}
+	}
 
-function renderQuestions(){
-	test = $("#test");
-	// $("#questionSpot").text("Question" + (pos+1) + "of" + questions.length);
-	question = questions[pos][0];
-	chA = questions[pos][1];
-	chB = questions[pos][2];
-	chC = questions[pos][3];
-	chD = questions[pos][4];
-	answer = questions[pos][5];
-	$("#questionSpot").html(question);
-	$("#choiceA").html(chA);
-	$("#choiceB").html(chB);
-	$("#choiceC").html(chC);
-	$("#choiceD").html(chD);
-}
+	function timesUp() { //function checks when time is up, then run this function 
+		$("#timeEnd").html("Timer: You ran out of time! Try Again!").css('color', 'red');
+		$("#unanswered").text("Unanswered Questions: " + unanswered++);
+		clearInterval(timerInterval);
+		pos++; //next question
+		renderQuestions();
+		reset();
+	}
+	
+	function reset() { //resets timer
+		clearInterval(timerInterval);
+		number = 11;
+		runTimer();
+	}
 
+	function renderQuestions() { //function to renderQuestion from questions array 
+		if (pos >=questions.length) { //run this line once last question is answered 
+			$("#finalResult").html("score " + correct + " out of " + questions.length);
+			pos = 0;
+			correct = 0;
+			return false; //this stops questions from rendering once last question hit
+			$("#timer").hide();
+		}
+		question = questions[pos][0];
+		chA = questions[pos][1];
+		chB = questions[pos][2];
+		chC = questions[pos][3];
+		chD = questions[pos][4];
+		answer = questions[pos][5];
+		$("#questionSpot").html(question);
+		$("#choiceA").html(chA);
+		$("#choiceB").html(chB);
+		$("#choiceC").html(chC);
+		$("#choiceD").html(chD);
+	}
 
-function checkAnswers(){
-	$('.buttonchoice').on('click', function() {
-  	var chosen = $(this).attr('value');
-	  	if (chosen == questions[pos][5]) {
-	  		correct++;
-	  		$("#correctAnswers").text("Correct Answers: " + correct);
-	  		reset();
-	  		//go to next question//renderQuestions(); ?????
-	  	}
-	  	else if (number === 0) {
-			$("#timeEnd").html("Timer: You ran out of time! Try Again!").css('color', 'red');
-			$("#unanswered").text("Unanswered Questions: " + unanswered++);
-			clearInterval(timerInterval);
-			reset();
+	function checkAnswers() {
+		$('.buttonchoice').on('click', function () {
+			var chosen = $(this).attr('value');
+			if (chosen == questions[pos][5]){
+				correct++;
+				$("#correctAnswers").text("Correct Answers: " + correct);
+				reset();
+			} 
+			else if (chosen !== questions[pos][5]){
+				incorrect++;
+				$("#incorrectAnswers").text("Incorrect Answers: " + incorrect);
+				reset();
 			}
-	  	else{
-	  		incorrect++;
-	  		console.log("incorrect answers: " + incorrect);
-	  		$("#incorrectAnswers").text("Incorrect Answers: " + incorrect);
-	  		reset();
-			//renderQuestions(); ?????
-	  	}
+			else {}
+			pos++;
+			renderQuestions();
+		});
+	}
 
-});
-}
+//=================Functions to run when start button clicked=================
 
+	$(".start").on("click", function(){
+		runTimer();
+		$(".start").hide();
+		//call functions here to run upon start button click 
+		renderQuestions();
+		checkAnswers();
+	});
+	//call functions here that should run independently 
 
-//need to figutr out how to loop through all questions 
-//THEN when all questions have been asked and answered, make the final screen in result box
+}); //document ready ends here
 
-
-renderQuestions();
-checkAnswers();
-
-}
-
-});
