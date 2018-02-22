b501123d707aeff521e556bbc26526d9e07d7d00
unknown <kerenshimoni6@gmail.com>
2018-02-22 13:11:25 -0800

    finalize code, complete readme

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Trivia-Game
 
+**Show off your Foodie Knowledge by playing my game!**
+[Click Here to Play!](https://shimonikeren.github.io/Trivia-Game/)
 
-//note to self: change timer to 20 sec
-//need to figutr out how to loop through all questions 
-//THEN when all questions have been asked and answered, make the final screen in result box
-
+This project uses _HTML, CSS, Bootstrap, Javascript, and jQuery_ to make a fun and interactive game with Food Themed Trvia Questions.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,27 +3,24 @@ correct = 0;
 incorrect = 0;
 unanswered = 1;
 var timerInterval;
-var number = 11;
+var number = 16;
 var pos = 0,
 	test, question, choice, choices, chA, chB, chC, chD, correct = 0;
 var questions = [
 //multidimensional array: each formatted as follows: [question, chA(choiceA), chB, chC, chD, answer]
-	// ["Dijon mustand originated in the city of Dijon, located in what country?", "England", "France", "USA", "Japan", "B"],
-	// ["<i>Granny Smith</i> is a popular type of which fruit?", "Pear", "Orange", "Apple", "Grape", "C"],
-	// ["What common kitchen item is made up of sodium and chlorine atoms?", "Pepper", "Vinegar", "Olive Oil", "Salt", "D"],
-	// ["Roquefort is a French blue cheese made from the milk of what animal?", "Sheep", "Goat", "Pig", "Cow", "A"],
-	// ["What is the colloquial term for a rotating tray often often placed on a table to aid in distributing food?", "Tired Susana", "Lazy Susan", "Lazy Jane", "Spinning Sally", "B"],
-	// ["Traditionally, the term <i>caviar</i> refers to the salt-cured roe of which fish?", "Sturgeon", "Trout", "Salmon", "Halibut", "A"],
-	// ["What is the main ingredient in thousand island dressing?", "Olive Oil", "Vinegar", "Mayonaise", "Relish", "C"],
+	["Dijon mustand originated in the city of Dijon, located in what country?", "England", "France", "USA", "Japan", "B"],
+	["<i>Granny Smith</i> is a popular type of which fruit?", "Pear", "Orange", "Apple", "Grape", "C"],
+	["What common kitchen item is made up of sodium and chlorine atoms?", "Pepper", "Vinegar", "Olive Oil", "Salt", "D"],
+	["Roquefort is a French blue cheese made from the milk of what animal?", "Sheep", "Goat", "Pig", "Cow", "A"],
+	["What is the colloquial term for a rotating tray often often placed on a table to aid in distributing food?", "Tired Susana", "Lazy Susan", "Lazy Jane", "Spinning Sally", "B"],
+	["Traditionally, the term <i>caviar</i> refers to the salt-cured roe of which fish?", "Sturgeon", "Trout", "Salmon", "Halibut", "A"],
+	["What is the main ingredient in thousand island dressing?", "Olive Oil", "Vinegar", "Mayonaise", "Relish", "C"],
 	["In which country did cheddar cheese originate?", "England", "France", "USA", "Japan", "A"],
 	["Sriracha is type of hot sauce named after a city located in what country?", "France", "Canada", "Thailand", "Japan", "C"],
 	["Champagne is a sparkling wine made from grapes grown in the Champagne region of which country?", "France", "Canada", "Thailand", "Japan", "A"]
 ];
 
-// =================Document Ready=================
-
 $('document').ready(function () {
-
 
 // =================Hidden elements to be rendered when called =================
 
@@ -34,11 +31,11 @@ $('document').ready(function () {
 	
 // =================Declare Functions =================
 
-	function runTimer() { //function to run a timer, decrease by 1 second 
+	function runTimer() { 
 		timerInterval = setInterval(decrement, 1000);
 	}
 
-	function decrement() { //function for seconds decreasing by 1  
+	function decrement() { 
 		number--;
 		$("#timer").html(number + " Seconds");
 		if (number === 0) {
@@ -46,7 +43,7 @@ $('document').ready(function () {
 		}
 	}
 
-	function timesUp() { //function checks when time is up, then run this function 
+	function timesUp() { 
   		$("#timeImg").fadeIn(500);
   		$("#timeImg").fadeOut(1000);
 		$("#unanswered").text("Unanswered Questions: " + unanswered++);
@@ -58,20 +55,19 @@ $('document').ready(function () {
 	
 	function reset() { //resets timer
 		clearInterval(timerInterval);
-		number = 11;
+		number = 16;
 		runTimer();
 	}
 
-
-	var resultCard = $(".resultCard");
+	var resultCard = $(".resultCard"); //create these two vars in order to swap the card divs in renderQuestion and restartGame functions 
 	var hiddenFinalCard = $(".hiddenFinalCard");
 
-	function restartGame(){ //restarts game when restart button clicked after game is over
+	function restartGame(){ //when "Play Again" button appears at end of game, this function restarts the game 
 		$("#restart").on("click", function(){
 			clearInterval(timerInterval);
 			reset(); //resets time 
 			$("#timer").fadeIn(500); //bring back timer 
-			$(".hiddenFinalCard").replaceWith(resultCard); //bring back original resultCard
+			$(".hiddenFinalCard").replaceWith(resultCard); //swap back to original resultCard
 			$(".resultCard").fadeIn(1000);
 			$("#wrongImg").hide();  //rehide images
 			$("#correctImg").hide(); 
@@ -80,16 +76,15 @@ $('document').ready(function () {
 			$("#correctAnswers").text("Correct Answers:"); //display blank answers
 			incorrect = 0;
 			$("#incorrectAnswers").text("Incorrect Answers:");
-			unanswered = 0;
+			unanswered = 1;
 			$("#unanswered").text("Unanswered Questions: ");
 			renderQuestions(); 
 			checkAnswers();
 		});
 	}
 
-
 	function renderQuestions() { //function to renderQuestion from questions array 
-		if (pos >=questions.length) { //run this line once last question is answered 
+		if (pos >=questions.length) { //this line is to run when this block of code when the last question has been answered
 			$("#finalResult").html("You got " + correct + " questions correct out of " + questions.length + " questions.");
 			$(".resultCard").replaceWith(hiddenFinalCard);
 			$(".hiddenFinalCard").fadeIn(1000);
@@ -99,7 +94,7 @@ $('document').ready(function () {
 			$("#timer").hide();//hides timer div
 			$(".buttonchoice").off("click");//turns off on click events
 			restartGame();
-			return false; //this exits the function 
+			return false; //this exits this part of the function 
 		}
 		question = questions[pos][0];  //the next few lines are grabbing questions and answers from the questions array
 		chA = questions[pos][1];
@@ -117,7 +112,7 @@ $('document').ready(function () {
 	function checkAnswers() {
 		$('.buttonchoice').on('click', function () {
 			var chosen = $(this).attr('value');
-			if (chosen == questions[pos][5]){ //compare chosen answer to assigned answer 
+			if (chosen == questions[pos][5]){ //compare chosen answer to assigned button value answer, then run if/else to determine if correct 
 				$("#correctImg").fadeIn(500);
   				$("#correctImg").fadeOut(1000);
 				correct++;
@@ -147,6 +142,4 @@ $('document').ready(function () {
 		checkAnswers();
 	});
 
-}); //document ready ends here
-
-		
+}); 
